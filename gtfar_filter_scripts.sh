a721c643af6c8d79f290c3b7ddb67c35bda24fd9
Mourad Souaiaia <souaiaia@hpc-uec-l.hpcc.usc.edu>
2014-02-19 16:25:30 -0800

    COOL

--- gtfar_filter_scripts.sh
+++ gtfar_filter_scripts.sh
@@ -16,7 +16,7 @@ TRIMARRAY=($TRIMS)
 
 function count_and_split_reads {
 
-    OUTDIR=$1; READS=$2
+    OUTDIR=$1; READS=$2; myPREFIX=$3
     CORES=$((  $(nproc) - 1 ))
     myEXT=${READS##*.}
     FILENAME=$(echo $READS | awk -F\. '{print $1}')
@@ -52,8 +52,8 @@ function count_and_split_reads {
         pre_filter_fastq.py "x"${NUMS[$k]} -t $TRIMSTR -p reads"$k" & k=$(( $k + 1 ))
     done 
     wait
-    #cat reads*_reject.fastq > ../reads.reject.fastq 
-    cat reads*.stats | sort -k1n,1 | awk '{if ($1==L) {A+=$2;B+=$3} else {if (NR>1) print L,A,B;L=$1;A=$2;B=$3}} END{print L,A,B}' > ../adaptor.stats 
+    cat reads*_reject.fastq > ../$myPREFIX.reject.fastq 
+    cat reads*.stats | sort -k1n,1 | awk '{if ($1==L) {A+=$2;B+=$3} else {if (NR>1) print L,A,B;L=$1;A=$2;B=$3}} END{print L,A,B}' > ../$myPREFIX.adaptor.stats 
 	printf "Success\n" 
     cd ../../
 }
