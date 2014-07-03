81cdbbcb1156ba87dbcd273fd672e3ee8e18620c
Rajiv Mayani <mayani@isi.edu>
2014-07-03 15:21:56 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,79 @@
+# Copyright 2007-2014 University Of Southern California
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def find_package_data(dirname):
+    def find_paths(dirname):
+        items = []
+        for fname in os.listdir(dirname):
+            path = os.path.join(dirname, fname)
+            if os.path.isdir(path):
+                items += find_paths(path)
+            elif not path.endswith('.py') and not path.endswith('.pyc'):
+                items.append(path)
+        return items
+
+    items = find_paths(dirname)
+    return [os.path.relpath(path, dirname) for path in items]
+
+
+setup(
+    name='pegasus-gtfar',
+    version='0.1',
+    author='Pegasus Team',
+    author_email='pegasus@isi.edu',
+    description='Pegasus REST API',
+    long_description=read('README.md'),
+    license='Apache2',
+    url='https://github.com/pegasus-isi/pegasus-gtfar',
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Application',
+        'Topic :: Scientific/Engineering :: Bio-informatics',
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix'
+    ],
+    packages=find_packages(),
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'pegasus-gtfar-server = pegasus.gtfar.server:main'
+        ]
+    },
+    install_requires=[
+        'argparse==1.2.1',
+        'Flask==0.10.1',
+        'Flask-Cache==0.13.1',
+        'Flask-SQLAlchemy==1.0',
+        'Werkzeug==0.9.6',
+        'Jinja2==2.7.3',
+        'SQLAlchemy==0.9.6',
+        'MySQL-python==1.2.5'
+    ],
+    test_suite='pegasus.tests'
+)
+
