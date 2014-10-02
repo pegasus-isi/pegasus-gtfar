174f6bdabd88c1a0a4103e3c48056f8b6a38beff
Rajiv Mayani <mayani@isi.edu>
2014-10-02 13:01:04 -0700

    Add method to check if directory exists

--- pegasus/gtfar/s3.py
+++ pegasus/gtfar/s3.py
@@ -63,6 +63,16 @@ class S3Utils(object):
                 except S3CreateError:
                     pass
 
+    def dir_exists(self, key):
+        key = key if key.endswith('/') else key + '/'
+
+        files = self._bucket.list(key)
+
+        for file in files:
+            return True
+
+        return False
+
     def get_bucket_name(self):
         return self._bucket.name
 
